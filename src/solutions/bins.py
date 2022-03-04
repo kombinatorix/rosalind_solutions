3a9208924eedc7a6295a3f2e6e86238dca60bac5
Sebastian Schönitz <sebastian.schoenitz@rwth-aachen.de>
2022-03-04 14:10:32 +0100

    add program for "bins"

---
+++ src/solutions/bins.py
@@ -0,0 +1,46 @@
+"""
+https://rosalind.info/problems/bins/
+"""
+from src.utils import io
+
+
+def binary_search(sorted_list_of_ints, searched_value):
+    left = 0
+    right = len(sorted_list_of_ints) - 1
+    middle = 0
+
+    while left <= right:
+
+        middle = (right + left) // 2
+
+        if sorted_list_of_ints[middle] < searched_value:
+            left = middle + 1
+
+        elif sorted_list_of_ints[middle] > searched_value:
+            right = middle - 1
+
+        else:
+            return middle + 1  # for index beginning with 1
+
+    return -1
+
+
+def values_present(search_for, search_in):
+    search_for = list(map(lambda x: int(x), search_for.split(" ")))
+    search_in = sorted(list(map(lambda x: int(x), search_in.split(" "))))
+
+    return [" ".join([str(binary_search(search_in, el)) for el in search_for])]
+
+
+if __name__ == "__main__":
+
+    data, filename = io.read_data("bins")
+    # data = [
+    #    "5",
+    #    "6",
+    #    "10 20 30 40 50",
+    #    "40 10 35 15 40 20",
+    # ]
+    solution = values_present(data[3], data[2])
+    # print(solution)
+    io.write_data(solution=solution, filename=filename)
